826254df6f71306f27d60042c422309c3bb8edca
sathish <sathish.jersy17@gmail.com>
2019-07-17 08:47:32 +0530

    1st time commit after modified

---
+++ src/test/java/Selenium_Framework/E2E_LakesEdge/Git_wo.java
@@ -0,0 +1,10 @@
+package Selenium_Framework.E2E_LakesEdge;
+
+public class Git_wo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello GITHUB");
+	}
+
+}
